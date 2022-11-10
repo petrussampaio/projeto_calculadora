18aaefc32c09f8641b2cb860fe2432fe0ee6d67f
PETRUS DAVI DE OLIVEIRA SAMPAIO <2211132@aluno.idp.edu.br>
2022-11-10 14:48:36 -0300

    Incluindo funcionalidade
    Incluindo commit que implementa a funcao de adicao

---
+++ calculadora.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void coleta_operandos(int qtd, int *num1, int *num2)
+{
+    printf("Digite o primeiro operando:");
+    scanf("%d%*c", num1);
+
+    if (qtd == 2)
+    {
+        printf("Digite o segundo operando:");
+        scanf("%d%*c", num2);
+    }
+}
+
+int soma(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int subtracao(int num1, int num2)
+{
+    return num1 - num2;
+}
+
+int main(void)
+{
+    char op;
+    int num1, num2, res, num_op;
+
+    while (1)
+    {
+        printf("Digite a operacao desejada (+, 0):");
+        scanf("%c%*c", &op);
+        num_op = 2;
+
+        switch (op)
+        {
+        case '+':
+            coleta_operandos(num_op, &num1, &num2);
+            res = soma(num1, num2);
+            break;
+
+        case '0':
+            return 0;
+
+        default:
+            printf("Operacao nao suportada. Digita novamente");
+        }
+
+        return 0;
+    }
+}
